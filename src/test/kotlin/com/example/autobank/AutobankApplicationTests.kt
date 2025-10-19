443aec95cf3d3941d489dfbb4f0f47257b167adc
Mads Hermansen <mdherman@stud.ntnu.no>
2025-10-19 14:52:23 +0200

    refactor: remove unused contextLoads test from AutobankApplicationTests

--- src/test/kotlin/com/example/autobank/AutobankApplicationTests.kt
+++ src/test/kotlin/com/example/autobank/AutobankApplicationTests.kt
@@ -6,11 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class AutobankApplicationTests {
     @Test
-    fun contextLoads() {
-        assert(false)
-    }
-
-    @Test
     fun anotherTest() {
         // Placeholder for additional tests
         assert(true)
