92f0daa2b0ba2e9535f494621cb038b73ea35e4d
mnyflot <mnyflot@gmail.com>
2026-02-08 16:24:39 +0100

    revert merge

--- src/main/kotlin/com/example/autobank/controller/ReceiptController.kt
+++ src/main/kotlin/com/example/autobank/controller/ReceiptController.kt
@@ -25,10 +25,10 @@ class ReceiptController {
             val res = receiptService.createReceipt(receipt)
             ResponseEntity.ok(res)
         } catch (e: Exception) {
-            println("Receipt creation failed: ${e.message}")
-            e.printStackTrace()
+            println(e)
             ResponseEntity.badRequest().build()
         }
+
     }
 
     @Operation(summary = "Get all receipts for user", description = "Retrieve a list of all receipts for the authenticated user")
