dedd6cee7b447eee04192384894b0499f6f8f8fc
AmundBerge <amundpamund@icloud.com>
2026-09-13 14:58:44 +0200

    #107 fetch imageUrl from OW-auth

--- src/main/kotlin/com/example/autobank/service/AuthenticationService.kt
+++ src/main/kotlin/com/example/autobank/service/AuthenticationService.kt
@@ -80,7 +80,7 @@ class AuthenticationService(
             val sub = getUserSub()
             // Return mock/minimal user data for local dev
             // Switch mail to your own email to avoid confusion when testing
-            return Auth0User(sub, "online.appkom@gmail.com", "Dev User")
+            return Auth0User(sub, "amund.dahlmo.berge@online.ntnu.no", "Dev User", "https://ca.slack-edge.com/T018UHUSG4E-U06LJ0DGCDB-d0914116e82e-512")
         }
 
         val endpoint = UriComponentsBuilder
@@ -107,7 +107,7 @@ class AuthenticationService(
         val user = response.body?.result?.data?.json
             ?: throw Exception("User not found")
 
-        return Auth0User(user.id, user.email, user.name)
+        return Auth0User(user.id, user.email, user.name, user.imageUrl)
     }
 
     fun fetchUserCommittees(): List<String> {
@@ -217,6 +217,7 @@ class AuthenticationService(
         data class User(
             val id: String,
             val email: String,
-            val name: String
+            val name: String,
+            val imageUrl: String? = null,
         )
 }
